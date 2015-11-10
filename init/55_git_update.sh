8ab6e036e124c85aafc16f2bfe1e3fbab59801ae
smdion <smdion@gmail.com>
2015-11-09 19:11:01 -0600

    fixed bad copy/paste

--- init/55_git_update.sh
+++ init/55_git_update.sh
@@ -3,6 +3,6 @@
 if [ ! -d "/app/plexemail/.git" ]; then
 /sbin/setuser abc git clone https://github.com/jakewaldron/PlexEmail.git /app/plexemail
 else
-cd /app/plexemail/.git
+cd /app/plexemail
 /sbin/setuser abc git pull
 fi
