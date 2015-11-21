c0a36ba859907b61de23822eb37a2501d35fb2fe
smdion <smdion@gmail.com>
2015-11-21 11:46:44 -0600

    move test scripts into subfolders

--- init/60_set_config.sh
+++ init/60_set_config.sh
@@ -13,12 +13,14 @@ if [ ! -f /app/plexemail/web/index.html ]; then
 	cp /config/www/index.html /app/plexemail/web/index.html
 fi
 
-if [ ! -f /config/send_one_time_email.sh ]; then
-  cp /defaults/send_one_time_email.conf /config/send_one_time_email.conf
+if [ ! -f /config/testscripts/send_one_time_email.sh ]; then
+	mkdir -p /config/testscripts/
+  cp /defaults/send_one_time_email.sh /config/testscripts/send_one_time_email.sh
 fi
 
-if [ ! -f /config/test_plexemail.sh ]; then
-  cp /defaults/test_plexemail.conf /config/test_plexemail.conf
+if [ ! -f /config/testscripts/test_plexemail.sh ]; then
+  mkdir -p /config/testscripts/
+  cp /defaults/test_plexemail.sh /config/testscripts/test_plexemail.sh
 fi
 
 chown abc:abc -R /config
