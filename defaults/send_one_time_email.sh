050a6ad950f2d422835671a5404e67c7f31d17f2
zaggash <root@62-210-240-108.online.net>
2016-06-13 13:15:15 +0200

    Reworked all the container  - Use smaller and easier webserver  - Change internal application port  - Add plex folder to the README  - Removed test files

--- defaults/send_one_time_email.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#This Script runs plexEmail and sends a one-time message to all reciepents.
-
-TEXT="CHANGE TEXT FOR EMAIL HERE"
-
-
-python /app/plexemail/scripts/plexEmail.py -c /config/config.conf -n "$TEXT"
--- defaults/test_plexemail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#This Script runs a test of plexEmail and sends a test email to the sender in /config/config.conf
-
-TEXT="CHANGE TEXT FOR EMAIL HERE"
-
-
-python /app/plexemail/scripts/plexEmail.py -t -c /config/config.conf
